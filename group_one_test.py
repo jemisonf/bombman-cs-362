c30d647dd5d6f22d1a08566f01a21072b86be589
richjoregonstate <richj@oregonstate.edu>
2019-06-02 20:15:01 -0700

    Ai & Positionable test hooks
    Adding hooks for my tests

--- group_one_test.py
+++ group_one_test.py
@@ -2,6 +2,8 @@ from menu_test import MenuTestCase
 from result_menu_test import ResultMenuTestCase
 from player_test import PlayerTestCase
 from bomb_test import ItemTestCase
+from ai_test import AITestCase
+from positionable_test import PositionableTestCase
 import unittest
 
 def main():
